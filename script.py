4b40662b13870ee2d75d49ddfa9fa4458306579e
Kenan Batur <30412755+Kenanbatur23@users.noreply.github.com>
2023-01-04 11:45:21 +0300

    add files

---
+++ script.py
@@ -0,0 +1,47 @@
+from pyautogui import write
+from pyautogui import press
+from pyautogui import hotkey
+from time import sleep
+print("welcome to the eagle script code for font setting")
+print("İMPORTANT: you must write '>' on command line of eagle ")
+size= input("what size should fonts on pcb be : ")
+print("click where the eagle window is, script is going to start in 5 seconds...")
+sleep(5)
+
+
+hotkey("ctrl","a")
+hotkey("ctrl","c")
+
+write("group all;")
+press("enter")
+
+write("smash (C")
+hotkey("ctrl","v")
+write("0 0);")
+press("enter")
+
+write("display none tname bname tval bval;")
+press("enter")
+
+write("group all;")
+press("enter")
+
+write("change font vector (C")
+hotkey("ctrl","v")
+write("0 0);")
+press("enter")
+
+write(f"change size {size}mil (C")
+hotkey("ctrl","v")
+write("0 0);")
+press("enter")
+
+write("change ratio 15 (C")
+hotkey("ctrl","v")
+write("0 0);")
+press("enter")
+
+write("display last;")
+press("enter")
+print("----beloved husband of zeynep----")
+sleep(0.5)
